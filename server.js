74cbd1ceb4fc5f63dfdce2d01cfae47268031100
Vladi756 <kosticv@lsbu.ac.uk>
2022-12-28 18:18:33 +0000

    404 URI routed

--- server.js
+++ server.js
@@ -8,12 +8,30 @@ const path = require('path')
 // which port the application is to run on
 const PORT = process.env.PORT || 3500
 
+// Routes
+
 // telling express where to find static files
-// __dirname is a global variable understood by express, tells it to 
-// search in this directory, then go to the 'public' directory
+// __dirname is a global variable understood by express, 
+// an environment variable that tells you the absolute path of the directory 
+// containing the currently executing file.
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
 
-// run the server 
+// A 'catch-all' that goes at the end to handle 404 responses. 
+// Like a default case in a switch statement
+app.all('*', (req, res) => {
+    res.status(404)
+    // handling json, html and other requests
+    if(req.accepts('html')) {
+        // route to 404 view
+        res.sendFile(path.join(__dirname, '/views/404.html'))
+    } else if(req.accepts('json')) {
+        res.json({message: '404: Not Found'})
+    } else {
+        res.type('txt').send('404: Not Found')
+    }
+})
+
+// Run the server 
 app.listen(PORT, () => console.log(`Running on port: ${PORT}.`))
